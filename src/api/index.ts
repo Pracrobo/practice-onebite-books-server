e629b1b7cdff271ebf8e200067ebf5f1c108f8e4
Pracrobo <cpu180621@gmail.com>
2025-09-16 10:20:35 +0900

    feat: vercel의 serverless 함수로 배포, import 해서 직접 호출안하게 변경

---
+++ src/api/index.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../../dist/src/app.module.js';
+import * as serverless from 'serverless-http';
+import express from 'express';
+
+const expressApp = express();
+const adapter = new ExpressAdapter(expressApp);
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, adapter);
+  app.enableCors();
+  await app.init();
+  return serverless(expressApp);
+}
+
+export const handler = async (req: any, res: any) => {
+  const server = await bootstrap();
+  return server(req, res);
+};
