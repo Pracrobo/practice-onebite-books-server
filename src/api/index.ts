5dc8146863caa0cd1574d8f83e8fd83e1212b14d
Pracrobo <cpu180621@gmail.com>
2025-09-16 13:21:07 +0900

    feat: 배포 관련 설명 주석 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
+import { ExpressAdapter } from '@nestjs/platform-express'; // @nestjs/platform-express의 ExpressAdapter 사용
 import { AppModule } from '../app.module.js';
 import * as serverless from 'serverless-http';
 import express from 'express';
@@ -18,3 +18,6 @@ export const handler = async (req: any, res: any) => {
   const server = await bootstrap();
   return server(req, res);
 };
+
+//ExpressAdapter를 사용하면 NestFactory가 Express를 올바르게 인식 → 타입 오류 제거
+// Serverless 환경에서도 바로 사용할 수 있음
